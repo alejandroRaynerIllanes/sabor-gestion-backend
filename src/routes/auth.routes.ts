2b1c1b3482b1822318c895658012434d39e801fe
Jairo-Main <jairomain14@gmail.com>
2026-04-25 22:47:40 -0400

    feat: Correccion del archivo auth.routes.ts, para pruebas en postman

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,10 @@
 //src/routes/auth.routes.ts
 import { Router } from 'express'
-import { loginUsuario } from '../controllers/auth.controller'
-import { Router } from 'express'
 import { loginUsuario, registrarUsuario } from '../controllers/auth.controller'
 
 const router = Router()
 
 router.post('/login', loginUsuario)
+router.post('/register', registrarUsuario)
 
 export default router
