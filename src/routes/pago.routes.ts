b58c5805ba18ecb9e9cdb639568fa15c090a1be3
alejandroRaynerIllanes <alejandroillanes42@gmail.com>
2026-05-21 01:40:40 -0400

    fix(email): cambio de puerto

--- src/routes/pago.routes.ts
+++ src/routes/pago.routes.ts
@@ -1,6 +1,11 @@
 // src/routes/pago.routes.ts
 import { Router } from 'express'
-import { generarPagoQR, procesarPagoFinal, simularPagoQR, enviarReciboCorreo } from '../controllers/pago.controller'
+import {
+  generarPagoQR,
+  procesarPagoFinal,
+  simularPagoQR,
+  enviarReciboCorreo
+} from '../controllers/pago.controller'
 import { verificarToken } from '../middlewares/auth.middleware'
 import { permitirRoles } from '../middlewares/rol.middleware'
 
@@ -10,11 +15,21 @@ const router = Router()
 router.post('/generar-qr/:pedidoId', verificarToken, generarPagoQR)
 
 // 2. Ruta principal que conecta con tu botón naranja de "Confirmar Pago"
-router.post('/:pedidoId/procesar', verificarToken, permitirRoles('Cajero', 'Administrador'), procesarPagoFinal)
+router.post(
+  '/:pedidoId/procesar',
+  verificarToken,
+  permitirRoles('Cajero', 'Administrador'),
+  procesarPagoFinal
+)
 
 // 3. NUEVA RUTA PÚBLICA: Escucha la señal del celular simulado (Sin verificarToken)
 router.post('/notificar-qr/:pedidoId', simularPagoQR)
 
-router.post('/:pedidoId/enviar-recibo', verificarToken, permitirRoles('Cajero', 'Administrador'), enviarReciboCorreo)
+router.post(
+  '/:pedidoId/enviar-recibo',
+  verificarToken,
+  permitirRoles('Cajero', 'Administrador'),
+  enviarReciboCorreo
+)
 
 export default router
